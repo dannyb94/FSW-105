8f0eb71b353894ee30ce86156db31aa7c2b68109
dannyb94 <danielle.ussery@bryanuniversity.edu>
2021-02-06 19:27:20 -0800

    Escape Room Simulation

--- Week6/Escape_Room_Simulation.js
+++ Week6/Escape_Room_Simulation.js
@@ -1,7 +1,7 @@
-const readline = require("./node_modules/readline-sync");
+const readline = require("./readline-sync");
 
 var playerName = readline.question("Enter your name: ");
-var welcomeMessage = "Hello " + playerName + ", welcome to the Escape Room Simulator!";
+var welcomeMessage = `Hello ${playerName}, welcome to the Escape Room Simulator!`;
 console.log(welcomeMessage);
 
 var isAlive = true;
@@ -11,20 +11,21 @@ while(isAlive == true){
     const menuOptions = readline.keyIn(' Enter 1 to place hand in the hole \n Enter 2 to find the key \n Enter 3 to open the door', {limit: '$<1-3>'});
     console.log(menuOptions);
     if(menuOptions == 1){
-        console.log(playerName + ", oh no you died. GAME OVER.");
+        console.log(`${playerName}, oh no you died. GAME OVER.`);
         isAlive = false;
     }
     else if(menuOptions == 2 && hasKey == false){
-        console.log(playerName + ", you found the missing key!");
+        console.log(`${playerName}, you found the missing key!`);
+        hasKey = true;
     }
     else if(menuOptions == 2 && hasKey == true){
-        console.log(playerName + ", you have already found the missing key. Proceed to the next option.");
+        console.log(`${playerName}, you have already found the missing key. Proceed to the next option.`);
     }
     else if(menuOptions == 3 && hasKey == false){
-        console.log(playerName + ", you have not found the missing key. Please go back and find the key.");
+        console.log(`${playerName}, you have not found the missing key. Please go back and find the key.`);
     }
     else if(menuOptions == 3 && hasKey == true){
-        console.log(playerName + ", you found the key and opened the door. You have escaped the room. Congratulations!");
+        console.log(`${playerName}, you found the key and opened the door. You have escaped the room. Congratulations!`);
         isAlive = false;
     }
 };
